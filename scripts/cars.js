c69dee15700436778abfb07adb503625104f74a0
LukeFitzGit <lukefitzgerald.ie@gmail.com>
2023-11-18 14:14:23 +0000

    typography and finished drivertips & buyacar

---
+++ scripts/cars.js
@@ -0,0 +1,82 @@
+const cars = [
+    {
+      image: "img/jose-carbajal-8xyki0bqvgw-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@jocac?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jose Carbajal</a> on <a href="https://unsplash.com/photos/blue-volkswagen-polo-5-door-hatchback-8xyki0bqvgw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "Volkswagen Golf",
+      name: "Volkswagen Golf",
+      description: "Very fancy car",
+      price: "21,500",
+      transmittion: "manual",
+      seats: "5"
+    },
+    {
+      image: "img/agustin-segura-eDXRq-2LfNM-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@agustin02?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Agustin Segura</a> on <a href="https://unsplash.com/photos/black-ford-mustang-coupe-parked-near-green-tree-eDXRq-2LfNM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "Ford GT",
+      name: "Ford GT",
+      description: "Very fancy car",
+      price: "90,000",
+      transmittion: "manual",
+      seats: "5"
+    },
+    {
+      image: "img/luka-tsikolia-ZLyzbhrfjMY-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@luka_tsikolia?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Luka TSikolia</a> on <a href="https://unsplash.com/photos/black-car-on-road-near-building-during-daytime-ZLyzbhrfjMY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "BMW M5",
+      name: "BMW M5",
+      description: "Very fancy car",
+      price: "29,000",
+      transmittion: "manual",
+      seats: "5"
+    },
+    {
+      image: "img/oguz-yagiz-kara-I-Q9kKf2MvU-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@oguzyagiz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Oğuz Yağız Kara</a> on <a href="https://unsplash.com/photos/white-audi-r-8-parked-on-roadside-during-daytime-I-Q9kKf2MvU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "mercedes benz",
+      name: "Mercedes Benz",
+      description: "Very fancy car",
+      price: "57000",
+      transmittion: "manual",
+      seats: "5"
+    },
+    {
+      image: "img/c-joyful-MPCKq5Amr9w-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@alonly?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">C Joyful</a> on <a href="https://unsplash.com/photos/red-audi-r-8-on-road-during-daytime-MPCKq5Amr9w?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "honda civic",
+      name: "Honda Civic",
+      description: "Very fancy car",
+      price: "52,000",
+      transmittion: "manual",
+      seats: "5"
+    },
+    {
+      image: "img/matt-henry-1nVbppFJl-s-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@neitmrh15?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Matt Henry</a> on <a href="https://unsplash.com/photos/white-tesla-parked-on-green-grass-lawn-during-day-time-1nVbppFJl-s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "Tesla S",
+      name: "Tesla S",
+      description: "Very fancy car",
+      price: "130,420",
+      transmittion: "automatic",
+      seats: "5"
+    },
+    {
+      image: "img/charlie-deets-AkgALppFIwo-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@charliedeets?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Charlie Deets</a> on <a href="https://unsplash.com/photos/white-sedan-parked-beside-mountain-during-daytime-AkgALppFIwo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "Tesla 3",
+      name: "Tesla 3",
+      description: "Very fancy car",
+      price: "100,420",
+      transmittion: "automatic",
+      seats: "5"
+    },
+    {
+      image: "img/kyle-reed-n5pFZHSTsKU-unsplash.jpg",
+      // Photo by <a href="https://unsplash.com/@kylereed?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kyle Reed</a> on <a href="https://unsplash.com/photos/black-sedan-n5pFZHSTsKU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      imageAlt: "Tesla X",
+      name: "Tesla X",
+      description: "Very fancy car",
+      price: "110,420",
+      transmittion: "automatic",
+      seats: "5"
+    },
+  ]
